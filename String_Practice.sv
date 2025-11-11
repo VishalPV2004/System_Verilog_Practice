37ed69e5924b9d8c557a3bf1fe949c0c3e2b7d34
VISHAL_PV <125368625+VishalPV2004@users.noreply.github.com>
2025-11-10 18:26:32 -0800

    Update String_Practice.sv

--- String_Practice.sv
+++ String_Practice.sv
@@ -12,3 +12,21 @@ module tb_string();
   end
 endmodule
     
+///////////////////////////////////////////////////////////////
+
+module tb_string();
+  string s;
+  string a, q;
+  int i = 24;
+  
+  initial begin 
+    //$dumpfile("dump.vcd"); $dumpvars;
+    s = "Hello";
+    a = "world";
+    
+    q = {s," ",a};
+    q = {q , "24"};
+    $display("%s",q);
+  end
+endmodule
+    
